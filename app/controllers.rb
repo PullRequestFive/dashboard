b180ee9d46df86654ffa4b197cd670d5fbfd69d4
Nat Welch <nat@natwelch.com>
2012-11-29 21:09:33 -0800

    return a /picture

--- app/controllers.rb
+++ app/controllers.rb
@@ -2,4 +2,17 @@ Dashboard.controllers  do
   get :index do
     render :index
   end
+
+  get :picture do
+    require 'rss'
+    require 'open-uri'
+
+    url = 'http://feeds.nationalgeographic.com/ng/photography/photo-of-the-day/'
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      big_img = feed.items.first.enclosure.url.sub("360x270", "1600x1200")
+
+      redirect big_img
+    end
+  end
 end
